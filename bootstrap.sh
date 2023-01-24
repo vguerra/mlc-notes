8fead0a6357c5bf7bd0f148cc95d1cf43c78ce8b
Victor Guerra <vguerra@gmail.com>
2023-01-24 22:57:20 +0100

    Adding bootstraping script to generate `nbdev` project.

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+nbdev_new --lib_name mlc-notes \
+	--nbs_path "./nbs" \
+	--git_url "https://github.com/vguerra/mlc-notes" \
+	--title "Machine Learning Compilation notes" \
+	--repo "mlc-notes" \
+	--branch "main" \
+	--user "vguerra" \
+	--author "Victor Guerra" \
+	--author_email "vguerra@gmail.com" \
+	--description "Machine Learning Compilation notes" \
+	--black_formatting true
+
+
